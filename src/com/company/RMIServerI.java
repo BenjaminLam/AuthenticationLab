0c3b42fb7e71fa9d1271d27c3bfcb9de417cee94
Benjamin Wrist Lam <Benjaminlam@me.com>
2018-10-31 16:28:55 +0100

    Framework for the server was created.

--- src/com/company/RMIServerI.java
+++ src/com/company/RMIServerI.java
@@ -5,4 +5,11 @@ import java.rmi.RemoteException;
 
 public interface RMIServerI extends Remote {
     public String print(String fileName, String printer) throws RemoteException;
+    public String queue();
+    public String topQueue(int job);
+    public String start();
+    public String restart();
+    public String status();
+    public String readConfig(String parameter);
+    public String setConfig(String parameter, String value);
 }
