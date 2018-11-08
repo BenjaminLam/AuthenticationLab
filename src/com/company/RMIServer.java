4a50ce197a33afe29b5eb46ddbdafd647d05ea3e
Benjamin Wrist Lam <Benjaminlam@me.com>
2018-11-08 21:18:01 +0100

    Added cryptography for hashing and salting password Also added check password and hash and store password using salted sha256

--- src/com/company/RMIServer.java
+++ src/com/company/RMIServer.java
@@ -2,10 +2,10 @@ package com.company;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
-import java.util.ArrayList;
-import java.util.Dictionary;
-import java.util.HashMap;
-import java.util.Map;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.*;
 
 public class RMIServer extends UnicastRemoteObject implements RMIServerI {
     //index in jobs array will be the jobnumber for each job.
@@ -139,4 +139,74 @@ public class RMIServer extends UnicastRemoteObject implements RMIServerI {
         configurations.put(parameter,value);
         return "Configuration for " + parameter + " was added";
     }
+
+    public byte[] generateSalt(){
+        SecureRandom random = new SecureRandom();
+        byte[] bytes = new byte[32];
+        random.nextBytes(bytes);
+        return bytes;
+    }
+
+    public byte[] saltPassword(byte[] salt, String password){
+        byte[] pwInBytes = Base64.getDecoder().decode(password);
+
+        byte[] saltPlusPw = new byte[salt.length + pwInBytes.length];
+
+        return saltPlusPw;
+    }
+
+    public boolean checkPassword(String userName, String password){
+        //Get salt and hash from storage
+        String[] saltAndHash = AuthenticatorFileReaderWriter.getPassword(userName);
+        //Extract salt and hash from string from storage
+        String salt = saltAndHash[1];
+        byte[] saltInByteAr = Base64.getDecoder().decode(salt);
+        String hashedPWFromStorage = saltAndHash[0];
+        // season input password with salt
+        byte[] saltedInputPW = saltPassword(saltInByteAr, password);
+        //hash salted input password
+        byte[] inputPwHashed = calculateHash(saltedInputPW);
+        // encode to base64
+        String inputPwHashedAsString = Base64.getEncoder().encodeToString(inputPwHashed);
+        //compare the two hashes
+        return inputPwHashedAsString.equals(hashedPWFromStorage);
+    }
+
+    private byte[] calculateHash(byte[] saltedInputPW) {
+        byte[] result = null;
+
+        try {
+            MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+            sha256.update(saltedInputPW);
+            result = sha256.digest();
+            return result;
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private boolean hashAndStorePassword(String userName, String password){
+        try{
+            // salt password
+            byte[] salt = generateSalt();
+            //prepare salt for storage by base64Decoding it
+            String saltAsString = Base64.getEncoder().encodeToString(salt);
+            //salt password
+            byte[] saltedHashedPW = saltPassword(salt,password);
+            //calculate hash of password
+            byte[] hashedPwBytes = calculateHash(saltedHashedPW);
+            //encode to string using base64
+            String hashedPwString = Base64.getEncoder().encodeToString(hashedPwBytes);
+            //store
+            AuthenticatorFileReaderWriter.setPassword(userName,hashedPwString,saltAsString);
+            return true;
+        } //GLHF with debugging
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
+
 }
